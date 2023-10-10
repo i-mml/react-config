5ac83da85c0a6ac13ceec0389c7a68d4a392aa50
maede <maedekarimi69110@gmail.com>
2023-10-10 13:59:26 +0330

    handle validaction yup and style to input box

--- src/views/loginView/components/informationBox.tsx
+++ src/views/loginView/components/informationBox.tsx
@@ -1,6 +1,12 @@
 import React from "react";
 import { Field, Form, Formik } from "formik";
 import s from "../login.module.scss";
+import * as Yup from "yup";
+
+const loginSchema = Yup.object().shape({
+  user_name: Yup.string().required("نام کاربری خود را وارد کنید"),
+  password: Yup.string().required("رمز خود را وارد کنید"),
+});
 
 const InformationBox = () => {
   return (
@@ -9,7 +15,9 @@ const InformationBox = () => {
         <div className={s.logoText}>نام برند و لوگو</div>
         <img src="/Images/logo.svg" alt="Logo" className={s.logoImg} />
       </div>
+
       <div className={s.form}>
+        <div className={s.welcome}>.به سبیان خوش آمدید</div>
         <Formik
           initialValues={{
             user_name: "",
@@ -18,27 +26,54 @@ const InformationBox = () => {
           onSubmit={() => {
             console.log("submit");
           }}
+          validationSchema={loginSchema}
         >
           {({ errors, touched }) => (
             <Form>
-              <Field
-                name="user_name"
-                placeholder="نام کاربری"
-                className={!errors.user_name ? s.field : s.errorField}
-              />
-              {errors.user_name && touched.user_name ? (
-                <div className={s.error}>{errors.user_name}</div>
-              ) : null}
+              <div className={s.loginInputBox}>
+                <div className={s.userNameField}>
+                  <Field
+                    name="user_name"
+                    placeholder="نام کاربری"
+                    className={s.userName}
+                  />
+                  <img
+                    src="/Images/Icons/user.svg"
+                    alt="user-icon"
+                    className={s.icon}
+                  />
+                  {errors.user_name && touched.user_name ? (
+                    <div className={s.error}>{errors.user_name}</div>
+                  ) : null}
+                </div>
 
-              <Field
-                name="password"
-                type="password"
-                placeholder="رمز عبور"
-                className={s.password}
-              />
-              {errors.password && touched.password ? (
-                <div className={s.error}>{errors.password}</div>
-              ) : null}
+                <div className={s.line}></div>
+
+                <div className={s.passwordField}>
+                  <Field
+                    name="password"
+                    type="password"
+                    placeholder="رمز عبور"
+                    className={s.password}
+                  />
+                  <img
+                    src="/Images/Icons/lock.svg"
+                    alt="user-icon"
+                    className={s.icon}
+                  />
+
+                  {errors.password && touched.password ? (
+                    <div className={s.error}>{errors.password}</div>
+                  ) : null}
+                </div>
+
+                <img
+                  src="/Images/Icons/eye.svg"
+                  alt="user-icon"
+                  className={s.eye}
+                />
+              </div>
+
               <button type="submit" className={s.button}>
                 ورود
               </button>
