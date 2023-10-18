3377165869c7c010a1d2e48faf9c29f1783b32a4
maede <maedekarimi69110@gmail.com>
2023-10-18 13:36:01 +0330

    handle password view in mobile and desktop mode

--- src/views/profileView/components/password/index.tsx
+++ src/views/profileView/components/password/index.tsx
@@ -1,7 +1,73 @@
 import React from "react";
 import s from "../../profile.module.scss";
+import { Field, Form, Formik } from "formik";
+
 const PasswordTab = () => {
-  return <div className={s.passwordContainer}>Password</div>;
+  return (
+    <div className={s.passwordContainer}>
+      <div className={s.form}>
+        <Formik
+          initialValues={{}}
+          onSubmit={() => {
+            "success";
+          }}
+        >
+          <Form className={s.formBody}>
+            <Field name="current-password">
+              {({ field }: any) => (
+                <div className={s.inputBox}>
+                  <div className={s.label}>رمز عبور فعلی</div>
+                  <input
+                    type="password"
+                    {...field}
+                    placeholder="رمز عبور فعلی خود را وارد کنید."
+                    className={s.input}
+                  />
+                </div>
+              )}
+            </Field>
+
+            <Field name="new-password">
+              {({ field }: any) => (
+                <div className={s.inputBox}>
+                  <div className={s.label}>رمز عبور جدید</div>
+                  <input
+                    type="password"
+                    {...field}
+                    placeholder="رمز عبور جدید خود را وارد کنید."
+                    className={s.input}
+                  />
+                </div>
+              )}
+            </Field>
+
+            <Field name="repeat-new-password">
+              {({ field }: any) => (
+                <div className={s.inputBox}>
+                  <div className={s.label}>تکرار رمز عبور جدید</div>
+                  <input
+                    type="password"
+                    {...field}
+                    placeholder="رمز عبور جدید خود را تکرار کنید."
+                    className={s.input}
+                  />
+                </div>
+              )}
+            </Field>
+
+            <div className={s.btnBox}>
+              <button type="submit" className={s.saveBtn}>
+                ثبت
+              </button>
+              <button type="submit" className={s.cancelBtn}>
+                لغو
+              </button>
+            </div>
+          </Form>
+        </Formik>
+      </div>
+    </div>
+  );
 };
 
 export default PasswordTab;
