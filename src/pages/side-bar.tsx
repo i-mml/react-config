4906685823c7b03f0394222d3514836542947c18
maede <maedekarimi69110@gmail.com>
2023-10-18 14:34:45 +0330

    handle side bar page

---
+++ src/pages/side-bar.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBarView from "../views/sideBarView";
+
+const SideBarPage = () => {
+  return (
+    <div>
+      <SideBarView />
+    </div>
+  );
+};
+
+export default SideBarPage;
--- src/routes/routingConfig.tsx
+++ src/routes/routingConfig.tsx
@@ -1,41 +1,59 @@
-import React from 'react'
-import LoginPage from '../pages/login'
-import { Routes, Route } from 'react-router-dom';
-import PrivateRoute from './privateRouter';
+import React from "react";
+import LoginPage from "../pages/login";
+import { Routes, Route } from "react-router-dom";
+import PrivateRoute from "./privateRouter";
 
-
-import NotFound from '../pages/notFound';
-import AbountUsPage from '../pages/about-us';
-import HomePage from '../pages/home';
-import ProfilePage from '../pages/profile';
-
+import NotFound from "../pages/notFound";
+import AbountUsPage from "../pages/about-us";
+import HomePage from "../pages/home";
+import ProfilePage from "../pages/profile";
+import SideBarPage from "../pages/side-bar";
 
 const RoutingConfig = () => {
-    return (
-        <Routes>
-            {/* public routes(e.g policies , login , 404 ,...) */}
-            <Route path='*' element={<NotFound />} />
-            <Route path='/login' element={<LoginPage />} />
+  return (
+    <Routes>
+      {/* public routes(e.g policies , login , 404 ,...) */}
+      <Route path="*" element={<NotFound />} />
+      <Route path="/login" element={<LoginPage />} />
 
-            {/* private routes (after login) */}
-            <Route
-                path='/'
-                element={<PrivateRoute>
-                    <HomePage />
-                </PrivateRoute>}
-            />
+      {/* private routes (after login) */}
+      <Route
+        path="/"
+        element={
+          <PrivateRoute>
+            <HomePage />
+          </PrivateRoute>
+        }
+      />
 
-            <Route
-                path='/profile'
-                element={
-                    <PrivateRoute>
-                        <ProfilePage />
-                    </PrivateRoute>
-                }
-            />
+      <Route
+        path="/profile"
+        element={
+          <PrivateRoute>
+            <ProfilePage />
+          </PrivateRoute>
+        }
+      />
 
-        </Routes>
-    )
-}
+      <Route
+        path="/side-bar"
+        element={
+          <PrivateRoute>
+            <SideBarPage />
+          </PrivateRoute>
+        }
+      />
 
-export default RoutingConfig
+      <Route
+        path="/about-us"
+        element={
+          <PrivateRoute>
+            <AbountUsPage />
+          </PrivateRoute>
+        }
+      />
+    </Routes>
+  );
+};
+
+export default RoutingConfig;
