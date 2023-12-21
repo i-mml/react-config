e1398590eaf28c7e47ad27894b64ea84c9b990bc
imml <m.javadi@mazdax.com>
2023-12-21 12:45:59 +0330

    handle ticket desktop table

--- src/views/supportView/index.tsx
+++ src/views/supportView/index.tsx
@@ -14,44 +14,67 @@ const SupportView = (props: ISupportViewProps) => {
     { id: 3, faTitle: "پشتیبانی داخلی", title: "internal_support" },
   ];
 
-  const tableList = [
+  const tableData = [
     {
       id: 1,
-      title: "شماره تیکت",
-      value: "",
-      icon: "",
-      subValue: "",
+      title: "مشکل در سفارش",
+      ticket_id: "65232",
+      created_at: "1403/09/03",
+      label: "high",
+      status: "pending",
+      updated_at: "1403/0903",
+      updated_at_user: "علی احمدی",
     },
     {
       id: 2,
-      title: "تاریخ ایجاد",
-      value: "",
-      icon: "",
-      subValue: "",
+      title: "مشکل در سفارش",
+      ticket_id: "65232",
+      created_at: "1403/09/03",
+      label: "high",
+      status: "pending",
+      updated_at: "1403/0903",
+      updated_at_user: "علی احمدی",
     },
     {
       id: 3,
-      title: "درجه اهمیت",
-      value: "",
-      icon: "",
-      subValue: "",
+      title: "مشکل در سفارش",
+      ticket_id: "65232",
+      created_at: "1403/09/03",
+      label: "medium",
+      status: "pending",
+      updated_at: "1403/0903",
+      updated_at_user: "علی احمدی",
     },
     {
       id: 4,
-      title: "وضعیت",
-      value: "",
-      icon: "",
-      subValue: "",
-    },
-    {
-      id: 5,
-      title: "آخرین آپدیت",
-      value: "",
-      icon: "",
-      subValue: "",
+      title: "مشکل در سفارش",
+      ticket_id: "65232",
+      created_at: "1403/09/03",
+      label: "low",
+      status: "pending",
+      updated_at: "1403/0903",
+      updated_at_user: "علی احمدی",
     },
   ];
 
+  const labelGenerator: any = {
+    high: {
+      class: s.high,
+      title: "فوری"
+    },
+    medium: {
+      class: s.medium,
+      title: "متوسط"
+    },
+    low: {
+      class: s.low,
+      title: "کم"
+    }
+  }
+
+  const statusGenerator: any = {
+    pending: "در حال بررسی"
+  }
 
 
   return (
@@ -74,23 +97,64 @@ const SupportView = (props: ISupportViewProps) => {
             </div>
           ))}
         </div>
-        <div className={s.content}>
-          <div className={s.internalContainer}>
-            {tableList.map((item) => {
-              return (
-                <table key={item.id}>
-                  <tr>
-                    <td>{item?.title}</td>
-                    <td>{item?.value}</td>
-                    <td>{item?.subValue}</td>
-                  </tr>
-                </table>
-              );
-            })}
-          </div>
-        </div>
-
       </div>
+
+      <table className={s.tableWrapper}>
+        <thead>
+          <tr>
+            <th>شماره تیکت</th>
+            <th>تاریخ ایجاد</th>
+            <th>درجه اهمیت</th>
+            <th>وضعیت</th>
+            <th>آخرین آپدیت</th>
+          </tr>
+        </thead>
+        <tbody className={s.tableBody}>
+          {
+            tableData?.map(item =>
+              <tr key={item.id}>
+                <td>
+                  <div className={s.ticketId}>
+                    <img src="/images/ticket_tag.png" className={s.ticketIcon} />
+                    <div>
+                      <div className={s.ticketIdNumber}>#{item.ticket_id}</div>
+                      <div className={s.ticketTitle}>
+                        {item.title}
+                      </div>
+                    </div>
+                  </div>
+                </td>
+                <td>
+                  {item?.created_at}
+                </td>
+                <td>
+                  <div className={`${s.label} ${labelGenerator[item?.label]?.class}`}>
+                    <span className={s.circle}></span>
+                    <span className={s.labelValue}>
+                      {labelGenerator[item?.label]?.title}
+                    </span>
+                  </div>
+                </td>
+                <td>
+                  {statusGenerator[item?.status]}
+                </td>
+                <td>
+                  <div className={s.updatedAt}>
+                    <div className={s.date}>
+                      {item?.updated_at}
+                    </div>
+                    <div className={s.updatedUser}>
+                      {item?.updated_at_user}
+                    </div>
+                  </div>
+                </td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
+
+
     </div>
   );
 };
