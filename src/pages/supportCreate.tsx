a6c61ea8f4e3183f2023efd333e113b87192216d
imml <m.javadi@mazdax.com>
2023-12-23 15:02:46 +0330

    handle add mobile nav to create support

--- src/pages/supportCreate.tsx
+++ src/pages/supportCreate.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import SupportCreateView from '../views/supportCreateView'
+import MobileNaviagtorLine from '../components/mobileNaviagatorLine'
 
 const SupportCreatePage = () => {
     return (
         <>
+            <MobileNaviagtorLine title="ارسال تیکت" link='/support' linkTitle='پشتیبانی' />
             <SupportCreateView />
         </>
     )
