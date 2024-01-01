967e98388e7f652b4a1004fb52b9b7cd5ae9355d
imml <m.javadi@mazdax.com>
2024-01-01 17:44:33 +0330

    handle line chart

--- src/components/charts/lineCart/index.tsx
+++ src/components/charts/lineCart/index.tsx
@@ -13,7 +13,58 @@ const LineChart = () => {
         },
         series: [
             {
-                data: [120, 200, 150, 80, 70, 110, 130],
+                data: [5, 20, 36, 10, 10, 20, 80, 65],
+                lineStyle: {
+                    color: '#4CA5FF' // Gray line color for shadow
+                },
+                itemStyle: {
+                    opacity: 0 // Make the shadow invisible
+                },
+                areaStyle: {
+                    color: '#B2DEFF80' // Gray color for shadow area
+                },
+                type: 'line',
+                datasetId: 'dataset_since_1950_of_germany',
+                showSymbol: false,
+                encode: {
+                    x: 'Year',
+                    y: 'Income',
+                    itemName: 'Year',
+                    tooltip: ['Income']
+                }
+            },
+            {
+                data: [120, 200, 150, 80, 70, 110, 130, 110],
+                lineStyle: {
+                    color: '#80BFFF' // Gray line color for shadow
+                },
+                itemStyle: {
+                    opacity: 0 // Make the shadow invisible
+                },
+                areaStyle: {
+                    color: '#B2DEFF80' // Gray color for shadow area
+                },
+                type: 'line',
+                datasetId: 'dataset_since_1950_of_germany',
+                showSymbol: false,
+                encode: {
+                    x: 'Year',
+                    y: 'Income',
+                    itemName: 'Year',
+                    tooltip: ['Income']
+                }
+            },
+            {
+                data: [140, 120, 88, 120, 130, 180, 60, 200],
+                lineStyle: {
+                    color: '#007eff' // Gray line color for shadow
+                },
+                itemStyle: {
+                    opacity: 0 // Make the shadow invisible
+                },
+                areaStyle: {
+                    color: '#B2DEFF80' // Gray color for shadow area
+                },
                 type: 'line',
                 datasetId: 'dataset_since_1950_of_germany',
                 showSymbol: false,
