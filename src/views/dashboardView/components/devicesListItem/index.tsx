425cfebc240ba632d9c68877c6d75401834ebc15
imml <m.javadi@mazdax.com>
2024-01-01 08:49:01 +0330

    edit online and offline condition in devices list dashboard page

--- src/views/dashboardView/components/devicesListItem/index.tsx
+++ src/views/dashboardView/components/devicesListItem/index.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import s from './style.module.scss';
 
 
-const DeviceListItem = ({ name = "", isLast = false, status = "" }) => {
+const DeviceListItem = ({ name = "", isLast = false, status = false }) => {
 
     return (
         <div className={`${s.container} ${isLast ? s.isLast : ""}`}>
             <img src='/images/icons/printer.svg' className={s.image} />
             <div className={s.title}>{name}</div>
 
-            <div className={`${s.statusBox} ${status === "offline" ? s.statusBoxOffline : ""}`}>
+            <div className={`${s.statusBox} ${!status ? s.statusBoxOffline : ""}`}>
                 <div className={s.statusBadge}></div>
-                <div className={s.status}>{status === "online" ? "آنلاین" : "آفلاین"}</div>
+                <div className={s.status}>{status ? "آنلاین" : "آفلاین"}</div>
             </div>
         </div>
     )
