d7c3a146cd53166d87301d47e2b3efd025886d3f
imml <m.javadi@mazdax.com>
2024-01-02 12:28:53 +0330

    handle line chart styles

--- src/components/charts/lineCart/index.tsx
+++ src/components/charts/lineCart/index.tsx
@@ -3,6 +3,20 @@ import s from './style.module.scss';
 import ReactEcharts from "echarts-for-react";
 import TitleBox from '../../../views/dashboardView/components/titleBox';
 
+const YearItem = ({ value, color }: { value: string, color: string }) => {
+    const colorClasses: any = {
+        "007eff": s.middleYear,
+        "4CA5FF": s.topYear,
+        "80BFFF": s.bottomYear
+    }
+    return (
+        <div className={s.year}>
+            <span>{value}</span>
+            <span className={`${s.badge} ${colorClasses[color]}`}></span>
+        </div>
+    )
+}
+
 const LineChart = () => {
     const [currentTab, setCurrentTab] = useState(1)
     const tabs = [
@@ -13,15 +27,17 @@ const LineChart = () => {
 
     const option = {
         xAxis: {
+
             type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+            data: ['فروردین', 'اردیبهشت', 'خرداد', 'تیر', 'مرداد', 'شهریور', 'مهر', "آبان", "آذر", "دی", "بهمن", "اسفند"]
         },
         yAxis: {
             type: 'value'
         },
+        center: ['60%', '40%'],
         series: [
             {
-                data: [5, 20, 36, 10, 10, 20, 80, 65],
+                data: [5, 20, 36, 10, 10, 20, 80, 65, 44, 180, 150, 120, 115, 189, 197, 148, 150, 150],
                 lineStyle: {
                     color: '#4CA5FF' // Gray line color for shadow
                 },
@@ -42,7 +58,7 @@ const LineChart = () => {
                 }
             },
             {
-                data: [120, 200, 150, 80, 70, 110, 130, 110],
+                data: [120, 200, 150, 80, 70, 110, 130, 110, 40, 80, 97, 155, 210, 100],
                 lineStyle: {
                     color: '#80BFFF' // Gray line color for shadow
                 },
@@ -63,7 +79,7 @@ const LineChart = () => {
                 }
             },
             {
-                data: [140, 120, 88, 120, 130, 180, 60, 200],
+                data: [140, 120, 88, 120, 130, 180, 60, 200, 190, 175, 164, 188, 191, 146,],
                 lineStyle: {
                     color: '#007eff' // Gray line color for shadow
                 },
@@ -101,7 +117,14 @@ const LineChart = () => {
                     )}
                 </div>
             </div>
-            <ReactEcharts option={option} />
+            <div className={s.years}>
+                <YearItem color='4CA5FF' value='1400' />
+                <YearItem color='80BFFF' value='1401' />
+                <YearItem color='007eff' value='1402' />
+            </div>
+            {/* @ts-ignore */}
+            <ReactEcharts option={option} style={{ width: "100%", }}
+            />
         </div >
     )
 }
