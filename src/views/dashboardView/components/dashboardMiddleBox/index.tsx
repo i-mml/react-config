21102dda362cbfebf0a4eca6b85b52b138db35e1
imml <m.javadi@mazdax.com>
2024-01-08 12:29:16 +0330

    hanlde internet chart values

--- src/views/dashboardView/components/dashboardMiddleBox/index.tsx
+++ src/views/dashboardView/components/dashboardMiddleBox/index.tsx
@@ -16,6 +16,8 @@ const DashboardMiddleBox = ({ data }: any) => {
     const user = useSelector((state: any) => state?.auth?.data?.user);
 
     const [donwloadValue, setDownloadValue] = useState(0)
+    const [uploadValue, setUploadValue] = useState(0)
+
 
     const option = {
         series: [
@@ -124,6 +126,7 @@ const DashboardMiddleBox = ({ data }: any) => {
 
     useEffect(() => {
         setDownloadValue(+data?.netSTatus?.data?.data?.channels?.find((item: any) => item?.name === "Traffic In")?.lastvalue.split(" ")[0]?.split(",")?.join("") || 0)
+        setUploadValue(+data?.netSTatus?.data?.data?.channels?.find((item: any) => item?.name === "Traffic Out")?.lastvalue.split(" ")[0]?.split(",")?.join("") || 0)
     }, [data?.netSTatus])
 
     return (
@@ -190,15 +193,15 @@ const DashboardMiddleBox = ({ data }: any) => {
                                 <svg xmlns="http://www.w3.org/2000/svg" width="63" height="14" viewBox="0 0 63 14" fill="none">
                                     <path d="M62.4492 0.813152C53.7433 1.95925 50.9021 6.36578 42.0234 6.86332C33.8377 7.32204 29.7232 3.09327 21.5977 3.83824C11.744 4.74164 11.1275 12.5858 1.17187 12.9135" stroke="#FE9B0E" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                                <p className={s.title}>Upload Mpbs</p>
-                                <p className={s.value}>48.2</p>
+                                <p className={s.title}>Upload Kpbs</p>
+                                <p className={s.value}>{uploadValue}</p>
                             </div>
                             <div className={s.section}>
                                 <svg xmlns="http://www.w3.org/2000/svg" width="60" height="14" viewBox="0 0 60 14" fill="none">
                                     <path d="M59.2227 0.813152C50.9313 1.95925 48.2254 6.36578 39.7695 6.86332C31.9736 7.32204 28.055 3.09327 20.3164 3.83824C10.932 4.74164 10.3448 12.5858 0.863281 12.9135" stroke="#007EFF" stroke-linecap="round" stroke-linejoin="round" />
                                 </svg>
-                                <p className={s.title}>Download Mpbs</p>
-                                <p className={s.value}>48.2</p>
+                                <p className={s.title}>Download Kpbs</p>
+                                <p className={s.value}>{donwloadValue}</p>
                             </div>
                         </div>
                     </div>
