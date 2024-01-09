426a2826bd00ce184ac94bc537ae7db5ecf0dc68
imml <m.javadi@mazdax.com>
2024-01-09 11:21:13 +0330

    handle pagination component

---
+++ src/components/pagination/index.tsx
@@ -0,0 +1,53 @@
+import React, { useEffect, useState } from 'react'
+import { Pagination, PaginationItem, PaginationLink } from 'reactstrap';
+import s from './style.module.scss';
+
+const TablePagination = ({ dataLength }: { dataLength: number }) => {
+    const pageSize = 20;
+    const [page, setPage] = useState(0)
+    const [maxPage, setMaxPage] = useState(6)
+
+    const handleClick = (e: React.MouseEvent<HTMLAnchorElement, MouseEvent>, index: number) => {
+        e.preventDefault();
+        setPage(index)
+    }
+
+    useEffect(() => {
+        setMaxPage(Math.ceil(dataLength / pageSize))
+    }, [dataLength])
+
+    return (
+        <div className={s.container}>
+            <Pagination aria-label="Page navigation example"  >
+
+                <PaginationItem disabled={page === 0}>
+                    <PaginationLink
+                        onClick={e => handleClick(e, page - 1)}
+                        next
+                        href="#"
+                    >
+                        <img src='/images/icons/paginationArrow.svg' className={s.prevIcon} />
+                    </PaginationLink>
+                </PaginationItem>
+                {[...Array(maxPage)].map((p, i) =>
+                    <PaginationItem active={i === page} key={i} className={s.item}>
+                        <PaginationLink onClick={e => handleClick(e, i)} href="#">
+                            {i + 1}
+                        </PaginationLink>
+                    </PaginationItem>
+                )}
+                <PaginationItem disabled={page === maxPage - 1}>
+                    <PaginationLink
+                        onClick={e => handleClick(e, page + 1)}
+                        previous
+                    >
+                        <img src='/images/icons/paginationArrow.svg' />
+                    </PaginationLink>
+                </PaginationItem>
+
+            </Pagination>
+        </div>
+    )
+}
+
+export default TablePagination
--- src/views/camerasView/index.tsx
+++ src/views/camerasView/index.tsx
@@ -1,37 +1,16 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import s from './style.module.scss';
 import TitleBox from '../dashboardView/components/titleBox';
 import CameraTableItem from './camerasTableItem';
 import { getCameraAll } from '../../api/services/camera';
 import { useQuery } from 'react-query';
 import InputSearch from '../../components/searchInput';
-import { isMobile } from 'react-device-detect';
+import TablePagination from '../../components/pagination';
+
 
 const CamerasView = () => {
     const { data, isLoading } = useQuery('get-all-camearas', getCameraAll);
-    const devicesTableData = [
-        {
-            id: 1,
-            name: "پرینتر",
-            nameEng: "webcam",
-            status: "ONLINE",
-            sensorLink: "https://google.com/"
-        },
-        {
-            id: 2,
-            name: "پرینتر",
-            nameEng: "webcam",
-            status: "ONLINE",
-            sensorLink: "https://google.com/"
-        },
-        {
-            id: 3,
-            name: "پرینتر",
-            nameEng: "webcam",
-            status: "OFFLINE",
-            sensorLink: "https://google.com/"
-        }
-    ]
+
     return (
         <div className={s.container}>
             <div className={s.titleWrappwer}>
@@ -54,6 +33,7 @@ const CamerasView = () => {
                     }
                 </tbody>
             </table>
+            <TablePagination dataLength={data?.length || 0} />
         </div>
     )
 }
