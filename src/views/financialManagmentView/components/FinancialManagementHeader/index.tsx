a086b163797e789db0279c82f38efbf128b97a99
imml <m.javadi@mazdax.com>
2024-01-09 14:08:50 +0330

    edit financial header title

--- src/views/financialManagmentView/components/FinancialManagementHeader/index.tsx
+++ src/views/financialManagmentView/components/FinancialManagementHeader/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import s from './style.module.scss';
 import moment from 'moment-jalaali';
+import TitleBox from '../../../dashboardView/components/titleBox';
 
 const FinancialManagementHeader = ({ title = "" }) => {
     const nowD = moment().format('jDD jMMM jYYYY (DD MMM YYYY)')
 
     return (
         <div className={s.header}>
-            <div className={s.title}>{title}</div>
+            <TitleBox title={title} />
 
             <div className={s.date}>
                 تاریخ امروز : {nowD}
