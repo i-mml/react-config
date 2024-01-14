d3b9116882cb759ebf609546c074f24159f119c7
imml <m.javadi@mazdax.com>
2024-01-14 12:16:09 +0330

    handle send message and create input

--- src/views/singleSupportView/index.tsx
+++ src/views/singleSupportView/index.tsx
@@ -1,21 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 import s from './style.module.scss';
 import ChatListItem from './components/chatListItem';
 import { useQuery } from 'react-query';
-import { getMessageAll } from '../../api/services/messages';
+import { getMessageAll, postMessageCreate } from '../../api/services/messages';
 import { useParams } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { Spinner } from 'reactstrap';
 
 const SingleSupportView = () => {
     const params = useParams()
-    const { data, isLoading } = useQuery("single-tickets-list", () => getMessageAll(`ticket_id=${params?.chatId}`));
+    const user = useSelector((state: any) => state?.auth?.data?.user);
+    const { data, isLoading, refetch } = useQuery("single-tickets-list", () => getMessageAll(`ticket_id=${params?.chatId}`));
+    const [messageValue, setMessageValue] = useState('');
+    const [loading, setLoading] = useState(false)
 
+    const hanldeSendMessage = async () => {
+        setLoading(true)
+        const formData = new FormData()
+        formData.append("content", messageValue)
+        formData.append("sender", user?.user_id)
+        formData.append("ticket_id", `${params?.chatId}`)
+        formData.append("type", "t")
 
+        postMessageCreate(formData).then(() => {
+            refetch();
+            setMessageValue("")
+            setLoading(false)
+
+        })?.catch(err => console.log(err))
+
+    }
+    console.log(data)
     return (
         <div className={s.container}>
             <div className={s.chatNumber}>
-                تیکت شماره {params?.id}
+                تیکت شماره {params?.chatId}
             </div>
             <div className={s.subTitle}>
                 این سابقه گفتگوی شما با نت پورت می باشد
@@ -25,6 +44,19 @@ const SingleSupportView = () => {
                 {isLoading ? <div className={s.spinnerBox}><Spinner color='primary' /></div> : data?.data?.length > 0 ? data?.data?.map((item: any) => <ChatListItem {...item} userFullName={data?.info?.user_name} />) : <div className={s.notFound}>
                     موردی یافت نشد.
                 </div>}
+            </div>
+            <div className={s.sendInputBox}>
+                <textarea
+                    value={messageValue} onChange={(e) => setMessageValue(e.target.value)}
+                    className={s.textArea} rows={1} placeholder='متن پیام خود را وارد کنید.'
+                    disabled={loading}
+                />
+
+                {
+                    loading ?
+                        <Spinner color='info' /> :
+                        <img alt='send' className={s.sendbtn} src="/images/icons/send.svg" onClick={loading ? () => { } : () => hanldeSendMessage()} />
+                }
             </div>
         </div>
     )
