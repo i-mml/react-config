f7ab7a4790381fbcde3301a3efa855ffbea9aab7
imml <m.javadi@mazdax.com>
2024-01-16 13:28:35 +0330

    edit sensor status condition

--- src/components/mapDeviceIcon/index.tsx
+++ src/components/mapDeviceIcon/index.tsx
@@ -37,7 +37,7 @@ const MapDeviceIcon = (props: any) => {
     return (
         <div className={s.mapDeviceIcon} onClick={toggle} {...props}  >
             <svg xmlns="http://www.w3.org/2000/svg" width="29" height="34" viewBox="0 0 29 34" fill="none" className={s.svgIcon} >
-                <path d="M14.5 33.5C18.125 26.8 29 22.4756 29 13.4C29 5.99938 22.5081 0 14.5 0C6.49187 0 0 5.99938 0 13.4C0 22.4756 10.875 26.8 14.5 33.5Z" fill={props?.sensors?.sensors?.find((node: any) => node?.sensor === "Ping")?.lastvalue === "0 msec" ? "#208B59" : "#C31414"} />
+                <path d="M14.5 33.5C18.125 26.8 29 22.4756 29 13.4C29 5.99938 22.5081 0 14.5 0C6.49187 0 0 5.99938 0 13.4C0 22.4756 10.875 26.8 14.5 33.5Z" fill={props?.sensors?.sensors?.find((node: any) => node?.sensor === "Ping")?.lastvalue === "0 msec" ? "#C31414" : "#208B59"} />
                 <circle cx="14.5" cy="13.5" r="10.5" fill="#E5F2FF" />
             </svg>
             {/* <img className={s.icon} src={} /> */}
