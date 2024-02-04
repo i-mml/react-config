798e82b204ad4f8f65522a4b0d2c20709754e879
imml <m.javadi@mazdax.com>
2024-02-04 23:40:59 +0330

    remove spiner in charts pie chart

--- src/views/dashboardView/components/chartsWrapper/index.tsx
+++ src/views/dashboardView/components/chartsWrapper/index.tsx
@@ -49,14 +49,14 @@ const ChartsWrapper = ({ data }: any) => {
         legend: {
             bottom: 0,
             left: 'center',
-            data: virtualMachines ? [virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore1"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 2"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 3"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 4 - 1T")]?.map((item: any) => item?.name) : []
+            data: virtualMachines?.device ? [virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore1"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 2"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 3"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 4 - 1T")]?.map((item: any) => item?.name) : []
         },
         series: [{
             type: 'pie',
             radius: '65%',
             selectedMode: "single",
             center: ['50%', '38%'],
-            data: virtualMachines ? [virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore1"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 2"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 3"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 4 - 1T")]?.map((item: any) => ({ value: +item?.info?.data[0]?.lastvalue?.split(" %")[0], name: item?.name })) : [],
+            data: virtualMachines?.device ? [virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore1"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 2"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 3"), virtualMachines?.device?.find((item: any) => item?.name === "Datastore Free: Datastore 4 - 1T")]?.map((item: any) => ({ value: +item?.info?.data[0]?.lastvalue?.split(" %")[0], name: item?.name })) : [],
             emphasis: {
                 itemStyle: {
                     shadowBlur: 10,
@@ -125,11 +125,8 @@ const ChartsWrapper = ({ data }: any) => {
             </div>
             <div className={s.smallBox}>
                 <TitleBox title='حجم دیسک سرور مجازی' />
-                {
-                    virtualMachines?.device?.length > 0 ?
-                        <ReactEcharts option={diskUsageOption} />
-                        : <Spinner color='primary' />
-                }
+
+                <ReactEcharts option={diskUsageOption} />
             </div>
 
         </div>
