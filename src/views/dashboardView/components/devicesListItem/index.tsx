0a90f113c2493e1462f5545da91ff6c6574c8514
imml <m.javadi@mazdax.com>
2024-02-17 16:57:58 +0330

    handle devices icon in dashbord devices list

--- src/views/dashboardView/components/devicesListItem/index.tsx
+++ src/views/dashboardView/components/devicesListItem/index.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import s from './style.module.scss';
+import { iconsTranslation } from '../../../../components/icons/iconsTranslation';
+import MonitorIcon from '../../../../components/icons/Monitor';
 
 
-const DeviceListItem = ({ name = "", isLast = false, status = false, icon = "" }) => {
+const DeviceListItem = ({ name = "", isLast = false, status = false, icon = "", group = "" }) => {
 
     return (
         <div className={`${s.container} ${isLast ? s.isLast : ""}`}>
-            <img src={'/images/icons/monitor.svg'} className={s.image} />
+            {iconsTranslation[group] || <MonitorIcon />}
             <div className={s.title}>{name}</div>
 
             <div className={`${s.statusBox} ${!status ? s.statusBoxOffline : ""}`}>
