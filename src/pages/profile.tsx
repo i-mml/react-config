1cce1e003d3516e61664a9c3f9c60eac918bdf48
imml <m.javadi@mazdax.com>
2024-02-17 12:18:55 +0330

    handle log out text in profile mobile

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -1,12 +1,36 @@
 import React from "react";
 import ProfileView from "../views/profileView";
 import MobileNaviagtorLine from "../components/mobileNaviagatorLine";
+import { useMutation } from "react-query";
+import { LogoutService } from "../api/services/auth";
+import { useNavigate, useParams } from "react-router-dom";
 
 const ProfilePage = () => {
+  const navigate = useNavigate()
+  const mutation = useMutation(() => LogoutService().finally(() => navigate("/login", { replace: true })));
+
+  const params = useParams()
+
+  const removeToken = () => {
+    mutation.mutate();
+  }
+
   return (
     <>
       <MobileNaviagtorLine title="حساب کاربری" hasLink={false} />
       <ProfileView />
+      <div
+        onClick={removeToken}
+        style={{
+          fontSize: "16px",
+          fontWeight: "600",
+          color: "#C31414",
+          textAlign: "center",
+          width: "100%",
+          marginTop: '40px'
+        }}>
+        خروج از حساب کاربری
+      </div>
     </>
   );
 };
