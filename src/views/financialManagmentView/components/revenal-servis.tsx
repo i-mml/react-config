9e63f70f458b6be419fab108c47c8f2f9bdb6a96
imml <m.javadi@mazdax.com>
2024-04-27 22:07:30 +0330

    chore: edit price field in revenal

--- src/views/financialManagmentView/components/revenal-servis.tsx
+++ src/views/financialManagmentView/components/revenal-servis.tsx
@@ -6,18 +6,16 @@ import FinancialManagementHeader from "./FinancialManagementHeader";
 import { useMutation, useQuery } from "react-query";
 import { postPaymentRequest } from "../../../api/services/payment";
 import { getSubscription } from "../../../api/services/subscription";
-import { getCompanyById } from "../../../api/services/company";
 
 const RenevalServis = () => {
   const authData = useSelector((state: any) => state?.auth?.data)
   const { data } = useQuery("subscription-data", getSubscription);
-  const { data: companyData } = useQuery("get-company-by-id", () => getCompanyById(authData?.admin?.company_Id));
 
   const informationList = [
     {
       id: 1,
       title: "اشتراک سرویس مانیتورینگ شبکه",
-      value: companyData?.data?.sub_scription + " تومان" ?? "-",
+      value: data?.data?.price + " تومان" ?? "-",
     },
     {
       id: 2,
