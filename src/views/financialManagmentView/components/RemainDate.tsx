027fa4602e4ef573d5834850e8554d5c80baceb8
imml <m.javadi@mazdax.com>
2024-04-30 15:30:24 +0330

    chore: change ramin date calculation formule

--- src/views/financialManagmentView/components/RemainDate.tsx
+++ src/views/financialManagmentView/components/RemainDate.tsx
@@ -1,7 +1,9 @@
 import s from '../financialManagment.module.scss';
 
 const RemainDate = ({ remainDay = 0, wholePeriod = 0 }: { remainDay: number, wholePeriod: number }) => {
-    const calculatedWdth = `${(wholePeriod - remainDay) / wholePeriod * 100}`
+    // const calculatedWdth = `${(wholePeriod - remainDay) / wholePeriod * 100}`
+    const calculatedWdth = `${remainDay / wholePeriod * 100}`
+
     return (
         <div className={s.remainBox} style={{
             background: `radial-gradient(closest-side, white 80%, transparent 80% 100%),conic-gradient(#003F80 ${calculatedWdth}%, #e7e7e7 0)`
