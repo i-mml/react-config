dc642c408cb3942f33f7e02cb2cd07cec7b7272a
imml <m.javadi@mazdax.com>
2024-05-12 16:41:24 +0330

    chore: handle banner description

--- src/views/dashboardView/components/middleSliderItem/index.tsx
+++ src/views/dashboardView/components/middleSliderItem/index.tsx
@@ -2,8 +2,10 @@ import React from 'react'
 import s from './style.module.scss';
 import { Link } from 'react-router-dom';
 
-const MiddleSliderItem = (props: { image: string, title: string, sub_title: string, external_link: string }) => {
-    const { image, title, sub_title: subTitle, external_link } = props
+const MiddleSliderItem = (props: {
+    image: string, title: string, sub_title: string, external_link: string, description: string
+}) => {
+    const { image, title, sub_title: subTitle, external_link, description } = props
 
     return (
         <Link className={s.middleSliderItem} to={external_link || ""} target='_blank'>
@@ -16,7 +18,7 @@ const MiddleSliderItem = (props: { image: string, title: string, sub_title: stri
                     {subTitle || "-"}
                 </div>
                 <div className={s.description}>
-                    راه حل ابری نت پورت یک سرویس مدیریت متمرکز است که به کاربران امکان می دهد تمام دستگاه های شبکه نت پورت خود را از طریق یک پلت فرم ساده و امن مدیریت کنند. کاربران می توانند دستگاه های نت پورت خود را از طریق رابط وب داشبورد نت پورت یا از طریق API ها استقرار، نظارت و پیکربندی کنند. هنگامی که کاربر تغییری در پیکربندی ایجاد می‌کند، درخواست تغییر به ابر نت پورت ارسال می‌شود و سپس به دستگاه(های) مربوطه فرستاده می‌شود.
+                    {description}
                 </div>
             </div>
         </Link>
--- src/views/guestDashboardView/components/middleSliderItem/index.tsx
+++ src/views/guestDashboardView/components/middleSliderItem/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import s from './style.module.scss';
 
-const MiddleSliderItem = ({ image, title, sub_title: subTitle }: { image: string, title: string, sub_title: string }) => {
+const MiddleSliderItem = ({ image, title, sub_title: subTitle, description }: { image: string, title: string, sub_title: string, description: string }) => {
     return (
         <div className={s.middleSliderItem}>
             <img src={image ? process.env.REACT_APP_IMAGE_BASE_URL + image : "/images/middleSlider.png"} className={s.image} />
@@ -13,7 +13,7 @@ const MiddleSliderItem = ({ image, title, sub_title: subTitle }: { image: string
                     {subTitle || "-"}
                 </div>
                 <div className={s.description}>
-                    راه حل ابری نت پورت یک سرویس مدیریت متمرکز است که به کاربران امکان می دهد تمام دستگاه های شبکه نت پورت خود را از طریق یک پلت فرم ساده و امن مدیریت کنند. کاربران می توانند دستگاه های نت پورت خود را از طریق رابط وب داشبورد نت پورت یا از طریق API ها استقرار، نظارت و پیکربندی کنند. هنگامی که کاربر تغییری در پیکربندی ایجاد می‌کند، درخواست تغییر به ابر نت پورت ارسال می‌شود و سپس به دستگاه(های) مربوطه فرستاده می‌شود.
+                    {description}
                 </div>
             </div>
         </div>
