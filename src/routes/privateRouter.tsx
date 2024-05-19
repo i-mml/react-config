248d04bd6afd3d0d67125a083a2a26f56d65f003
imml <m.javadi@mazdax.com>
2024-05-19 10:25:38 +0330

    feat: handle subsription validation in private route

--- src/routes/privateRouter.tsx
+++ src/routes/privateRouter.tsx
@@ -1,16 +1,22 @@
 import Cookies from "js-cookie";
 import React from "react";
 import type { ReactElement } from "react";
-import { Navigate } from "react-router-dom";
+import { Navigate, useLocation } from "react-router-dom";
 import Layout from "../views/layout/layout";
+import { useSelector } from "react-redux";
 
 
 
 
 function PrivateRoute({ children }: { children: ReactElement }) {
+  const authData = useSelector((state: any) => state?.auth?.data)
+  const location = useLocation()
+
   const tokenValue = Cookies.get("access-token")
 
   if (!tokenValue || tokenValue === "") { return <Navigate to='/login' replace /> };
+  if (!authData?.subscription && location.pathname !== "/financial-management") { return <Navigate to='/financial-management' /> };
+
 
   return <Layout>
     {children}
